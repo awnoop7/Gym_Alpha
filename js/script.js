bfb3f52532defa751a6a77da6515d063ae3fc62e
awnoop7 <awnnoopp7@gmail.com>
2024-11-07 12:16:43 +0545

    made some changes in navbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 document.getElementById("btn").addEventListener("click",function(){
     window.location.href = "Assets/Login&Signin/login.html";
 })
+
+
+// navbar function
+document.getElementById("showNavbarBtn").onclick = function() {
+    document.getElementById("nav").classList.toggle("show");
+};
