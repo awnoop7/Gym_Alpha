a1654cba346ed5226499d5f4ce7e2b4f3b1ebdde
awnoop7 <awnnoopp7@gmail.com>
2024-11-11 15:51:43 +0545

    added more section

--- js/script.js
+++ js/script.js
@@ -4,10 +4,22 @@ document.getElementById("btn").addEventListener("click",function(){
 
 
 // navbar function
-document.getElementById("showNavbarBtn").onclick = function() {
-    document.getElementById("nav").classList.toggle("show");
-};
+document.getElementById('hamburger-btn').addEventListener('click', function() {
+    const navLinks = document.getElementById('r_nav');
+    // Toggle the 'active' class to open and close the navigation menu
+    navLinks.classList.toggle('active');
+});
 
 
 
 
+
+
+
+// contact_us
+
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Thank you for reaching out! We will get back to you soon.');
+    this.reset();
+});
